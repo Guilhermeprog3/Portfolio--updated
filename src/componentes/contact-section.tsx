6aa3ef0945e21c00518b76c5a3b872a041fc44fb
Gui Rios <guilhermeriosprog@gmail.com>
2025-08-02 12:54:02 -0300

    fix:corrigindo linkedin

--- src/componentes/contact-section.tsx
+++ src/componentes/contact-section.tsx
@@ -331,7 +331,7 @@ export function ContactSection() {
                   </motion.a>
 
                   <motion.a
-                    href="www.linkedin.com/in/guilherme-s-rios-dev"
+                    href="https://www.linkedin.com/in/guilherme-s-rios-dev"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="group relative"
